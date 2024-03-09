257ab65af4442340af8fe4becfe5db9c4f1ebf7b
João Martins <jmbaltazarmartins@gmail.com>
2024-03-09 14:03:32 +0000

    implementacao funcao done

--- functions/index.js
+++ functions/index.js
@@ -5,35 +5,21 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addmessage = functions.https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await admin.firestore()
-      .collection("messages")
-      .add({original: original});
-  // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+// Function to check if a number is prime
+function isPrime(num) {
+  for(let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++)
+    if(num % i === 0) return false; 
+  return num > 1;
+}
+
+// Cloud Function to generate prime numbers less than 1000
+exports.generatePrimes = functions.https.onRequest(async (req, res) => {
+  let primes = [];
+  for(let i = 2; i < 1000; i++) {
+    if(isPrime(i)) {
+      primes.push(i);
+    }
+  }
+  res.json({result: primes});
 });
 
-// Listens for new messages added to /messages/:documentId/original
-// and saves an uppercased version of the message
-// to /messages/:documentId/uppercase
-exports.makeuppercase = functions.firestore.document("/messages/{documentId}")
-    .onCreate((snap, context) => {
-      // Grab the current value of what was written to Firestore.
-      const original = snap.data().original;
-
-      // Access the parameter `{documentId}` with `context.params`
-      console.log("Uppercasing", context.params.documentId, original);
-
-      const uppercase = original.toUpperCase();
-
-      // You must return a Promise when performing
-      // asynchronous tasks inside a function
-      // such as writing to Firestore.
-      // Setting an 'uppercase' field in Firestore document returns a Promise.
-      return snap.ref.set({uppercase}, {merge: true});
-    });
