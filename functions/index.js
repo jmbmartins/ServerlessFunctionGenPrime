d5c90adb75925032480c888217f9e12c54e62e19
João Martins <jmbaltazarmartins@gmail.com>
2024-03-09 22:17:14 +0000

    .

--- functions/index.js
+++ functions/index.js
@@ -5,21 +5,32 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// Function to check if a number is prime
+/**
+ * Function to check if a number is prime
+ * @param {number} num - The number to check
+ * @return {boolean} - Whether the number is prime
+ */
 function isPrime(num) {
-  for(let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++)
-    if(num % i === 0) return false; 
+  for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
   return num > 1;
 }
 
 // Cloud Function to generate prime numbers less than 1000
 exports.generatePrimes = functions.https.onRequest(async (req, res) => {
-  let primes = [];
-  for(let i = 2; i < 1000; i++) {
-    if(isPrime(i)) {
+  const primes = [];
+  const startTime = Date.now();
+  for (let i = 2; i < 1000; i++) {
+    if (isPrime(i)) {
       primes.push(i);
     }
   }
   res.json({result: primes});
+
+  const endTime = Date.now();
+  console.log(`Function execution time: ${endTime - startTime}ms`);
 });
 
